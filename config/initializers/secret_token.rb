c7ea5f6d24b902ede5c506a7144a29e4be5225ea
Ozzuha <asska27@gmail.com>
2013-11-09 18:44:51 +0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Ozzuha::Application.config.secret_key_base = '758ff0bca2adb48edf13eb2ee33d59db4a560db4ee42e68407ec467f888a906ce0d0c877a1a1d3069203e8cca3fb72df1dd05d507be12dbc3fd6dcc764bb783d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Ozzuha::Application.config.session_store :cookie_store, key: '_ozzuha_session'
